3f1844efeb8081d1f1cf47590af4b178f5dfd1a5
Arij Achour <arijachour87@gmail.com>
2023-10-26 00:23:17 +0100

    gvehf

--- install_git.sh
+++ install_git.sh
@@ -2,4 +2,10 @@ sudo apt install git
 sudo apt install python3
 sudo apt install pip 
 pip install psutil
+sudo apt-get install -y systemd
+
 git clone https://github.com/ArijAchour/code-
+sudo cp test.service /etc/systemd/system
+sudo systemctl daemon-reload
+sudo systemctl enable test.service
+sudo systemctl start test.service
